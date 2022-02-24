e5079388ac317079bfbf46126ee0cf54aba87fd4
qinmb812 <2449338367@qq.com>
2022-02-24 22:47:56 +0800

    SpringBoot入门--使用向导快速创建SpringBoot应用

---
+++ springboot01_helloworld_quick/src/main/java/com/at/springboot/Springboot01HelloworldQuickApplication.java
@@ -0,0 +1,11 @@
+package com.at.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot01HelloworldQuickApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot01HelloworldQuickApplication.class, args);
+    }
+}
---
+++ springboot01_helloworld_quick/src/test/java/com/at/springboot/Springboot01HelloworldQuickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.at.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01HelloworldQuickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
