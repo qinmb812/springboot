5aeb2c8d7f896e169bbb2db5b5744146bd68e71e
qinmb812 <2449338367@qq.com>
2022-02-25 21:21:51 +0800

    SpringBoot入门--配置文件

---
+++ springboot02_config/src/main/java/com/at/springboot/bean/Person.java
@@ -0,0 +1,113 @@
+package com.at.springboot.bean;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Email;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 将配置文件中配置的每一个属性的值，映射到这个组件中
+ *
+ * @ConfigurationProperties： 告诉SpringBoot将本类中的所有属性和配置文件中相关的配置进行绑定
+ * prefix = "person"： 配置文件中哪个下面的所有属性进行一一映射
+ * <p>
+ * 只有这个组件是容器中的组件，才能使用容器提供的@ConfigurationProperties功能
+ */
+@Component
+@ConfigurationProperties(prefix = "person")
+@Validated
+public class Person {
+    /**
+     * <bean class="Person">
+     * <properties name="lastName" value="字面量/${key}从环境变量、配置文件中获取值/#{SpEL}></properties>
+     * </bean>
+     */
+
+    // lastName必须是邮箱格式
+    //    @Email
+    //    @Value("${person.last-name}")
+    private String lastName;
+    //    @Value("#{12*3}")
+    private Integer age;
+    //    @Value("true")
+    private boolean boss;
+    private Date birth;
+
+    private Map<String, Object> maps;
+    private List<Object> lists;
+    private Dog dog;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public boolean isBoss() {
+        return boss;
+    }
+
+    public void setBoss(boolean boss) {
+        this.boss = boss;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public Map<String, Object> getMaps() {
+        return maps;
+    }
+
+    public void setMaps(Map<String, Object> maps) {
+        this.maps = maps;
+    }
+
+    public List<Object> getLists() {
+        return lists;
+    }
+
+    public void setLists(List<Object> lists) {
+        this.lists = lists;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", boss=" + boss +
+                ", birth=" + birth +
+                ", maps=" + maps +
+                ", lists=" + lists +
+                ", dog=" + dog +
+                '}';
+    }
+}
---
+++ springboot02_config/src/test/java/com/at/springboot/Springboot02ConfigApplicationTests.java
@@ -0,0 +1,23 @@
+package com.at.springboot;
+
+import com.at.springboot.bean.Person;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * SpringBoot单元测试
+ *
+ * 可以在测试期间很方便的类似编码一样进行自动注入等容器功能
+ */
+@SpringBootTest
+class Springboot02ConfigApplicationTests {
+    @Autowired
+    Person person;
+
+    @Test
+    void contextLoads() {
+        System.out.println(person);
+    }
+
+}
