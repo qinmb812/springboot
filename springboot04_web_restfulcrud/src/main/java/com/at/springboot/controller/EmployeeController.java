e79edcf84dd225629414b9a1d1ffe234efac6da4
qinmb812 <2449338367@qq.com>
2022-03-02 21:41:12 +0800

    SpringBoot之web开发--【实验】--员工删除之修删除完成

--- springboot04_web_restfulcrud/src/main/java/com/at/springboot/controller/EmployeeController.java
+++ springboot04_web_restfulcrud/src/main/java/com/at/springboot/controller/EmployeeController.java
@@ -7,10 +7,7 @@ import com.at.springboot.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -68,6 +65,14 @@ public class EmployeeController {
     public String updateEmployee(Employee employee) {
         System.out.println("修改的员工数据：" + employee);
         employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    // 员工删除
+    @DeleteMapping("/emp/{id}")
+    public String deleteEmployee(@PathVariable("id") Integer id) {
+        System.out.println("删除的员工id：" + id);
+        employeeDao.delete(id);
         return "redirect:/emps";
     }
 }
