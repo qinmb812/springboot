a11fbf58831c8b14a9848764e7018063f42b9f6a
qinmb812 <2449338367@qq.com>
2022-03-05 10:33:55 +0800

    SpringBoot之web开发--外部Servlet容器

---
+++ springboot04_web_jsp/src/test/java/com/at/springboot/Springboot04WebJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.at.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot04WebJspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
