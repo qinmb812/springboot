c08f77a79476faa367b6f01d574545096f9c8aa1
qinmb812 <2449338367@qq.com>
2022-03-06 21:12:04 +0800

    SpringBoot之数据访问--整合Mybatis--注解版Mybatis

--- springboot06_data_mybatis/src/main/java/com/at/springboot/Springboot06DataMybatisApplication.java
+++ springboot06_data_mybatis/src/main/java/com/at/springboot/Springboot06DataMybatisApplication.java
@@ -1,8 +1,11 @@
 package com.at.springboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 使用MapperScan批量扫描所有的mapper接口
+//@MapperScan("com.at.springboot.mapper")
 @SpringBootApplication
 public class Springboot06DataMybatisApplication {
 
---
+++ springboot06_data_mybatis/src/main/java/com/at/springboot/controller/DeptController.java
@@ -0,0 +1,25 @@
+package com.at.springboot.controller;
+
+import com.at.springboot.bean.Department;
+import com.at.springboot.mapper.DepartmentMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeptController {
+    @Autowired
+    DepartmentMapper departmentMapper;
+
+    @GetMapping("/dept/{id}")
+    public Department getDepartment(@PathVariable Integer id) {
+        return departmentMapper.getDeptById(id);
+    }
+
+    @GetMapping("/dept")
+    public Department insertDept(Department department) {
+        departmentMapper.insertDept(department);
+        return department;
+    }
+}
