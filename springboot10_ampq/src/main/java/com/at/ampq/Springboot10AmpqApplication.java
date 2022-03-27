edc1c09284a327ab4232a8aad6307e4f96e913c8
qinmb812 <2449338367@qq.com>
2022-03-27 21:29:08 +0800

    SpringBoot之消息--RabbitTemplate发送接受消息&序列化机制

---
+++ springboot10_ampq/src/main/java/com/at/ampq/Springboot10AmpqApplication.java
@@ -0,0 +1,19 @@
+package com.at.ampq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 自动配置
+ * 1、RabbitAutoConfiguration
+ * 2、有自动配置了连接工厂ConnectionFactory
+ * 3、RabbitProperties封装了RabbitMQ的配置
+ * 4、RabbitTemplate：给RabbitMQ发送和接收消息
+ * 5、AmqpAdmin：RabbitMQ系统管理功能组件
+ */
+@SpringBootApplication
+public class Springboot10AmpqApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot10AmpqApplication.class, args);
+    }
+}
---
+++ springboot10_ampq/src/test/java/com/at/ampq/Springboot10AmpqApplicationTests.java
@@ -0,0 +1,53 @@
+package com.at.ampq;
+
+import com.at.ampq.bean.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootTest
+class Springboot10AmpqApplicationTests {
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    /**
+     * 1、单播（点对点）
+     */
+    @Test
+    void contextLoads() {
+        // Message需要自己构造一个；定义消息体内容和消息头
+        // rabbitTemplate.send(exchange,routeKey,message);
+
+        // object默认当成消息体，只需要传入要发送的对象，自动序列化发送给rabbitmq；
+        // rabbitTemplate.convertAndSend(exchange,routeKey,object);
+//        Map<String, Object> map = new HashMap<>();
+//        map.put("msg", "这是第一个消息");
+//        map.put("data", Arrays.asList("helloworld", 123, true));
+        // 对象被默认序列化以后发送出去
+//        rabbitTemplate.convertAndSend("exchange.direct", "at.news", map);
+        rabbitTemplate.convertAndSend("exchange.direct", "at.news", new Book("西游记", "吴承恩"));
+    }
+
+    /**
+     * 接受数据
+     */
+    @Test
+    void receive() {
+        Object o = rabbitTemplate.receiveAndConvert("at.news");
+        System.out.println(o.getClass());
+        System.out.println(o);
+    }
+
+    /**
+     * 2、广播
+     */
+    @Test
+    void sendMsg() {
+        rabbitTemplate.convertAndSend("exchange.fanout", "", new Book("三国演义", "罗贯中"));
+    }
+}
