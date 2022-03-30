3d4f3d31f70d1bb33b655eec2a9a7f454b7793a9
qinmb812 <2449338367@qq.com>
2022-03-30 20:36:41 +0800

    SpringBoot之检索--整合SpringBoot Data Elasticsearch

--- springboot11-elastic/src/main/java/com/at/elastic/repository/BookRepository.java
+++ springboot11-elastic/src/main/java/com/at/elastic/repository/BookRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 import java.util.List;
 
 public interface BookRepository extends ElasticsearchRepository<Book, Integer> {
+    // 参照   https://docs.spring.io/spring-data/elasticsearch/docs/current/reference/html/#reference
     List<Book> findByBookNameLike(String bookName);
 }
