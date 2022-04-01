fe77bc6353ca65ecbb77752260e1d1f86b95d33b
qinmb812 <2449338367@qq.com>
2022-04-01 21:33:09 +0800

    SpringBoot之安全--记住我&定制登录页

--- springboot13-security/src/main/java/com/at/security/config/MySecurityConfig.java
+++ springboot13-security/src/main/java/com/at/security/config/MySecurityConfig.java
@@ -17,15 +17,21 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/level2/**").hasRole("VIP2")
                 .antMatchers("/level3/**").hasRole("VIP3");
         // 开启自动配置的登录功能，效果：如果没有登录，没有权限就会来到登录页面
-        http.formLogin();
+        http.formLogin().usernameParameter("user").passwordParameter("pwd").loginPage("/userlogin");
         // 1、/login来到登录页
         // 2、重定向到/login?error表示登录失败
         // 3、更多详细规定
+        // 4、默认post形式的/login代表处理登录
 
         // 开启自动配置的注销功能
         http.logout().logoutSuccessUrl("/");    // 注销成功以后来到首页
         // 1、访问/logout表示用户注销，清空session
         // 2、注销成功会返回/login?logout页面
+
+        // 开启记住我功能
+        http.rememberMe().rememberMeParameter("remember");
+        // 登录成功以后，将cookie发给浏览器保存，以后访问页面带上这个cookie，只要通过检查就可以免登录
+        // 点击注销会删除cookie
     }
 
     /**
