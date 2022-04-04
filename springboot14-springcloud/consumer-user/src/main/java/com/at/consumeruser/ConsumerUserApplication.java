31e891eb491bf5aaa0c41b278a58c8bdea1ebdf6
qinmb812 <2449338367@qq.com>
2022-04-04 21:09:17 +0800

    SpringBoot之分布式--服务注册

--- springboot14-springcloud/consumer-user/src/main/java/com/at/consumeruser/ConsumerUserApplication.java
+++ springboot14-springcloud/consumer-user/src/main/java/com/at/consumeruser/ConsumerUserApplication.java
@@ -2,12 +2,12 @@ package com.at.consumeruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient  // 开启
 @SpringBootApplication
 public class ConsumerUserApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ConsumerUserApplication.class, args);
     }
-
 }
