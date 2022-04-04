9bb3ee1c0c9a6fe6620d8aab875427e496ca1b03
qinmb812 <2449338367@qq.com>
2022-04-04 20:43:40 +0800

    SpringBoot之分布式--SpringCloud--Eureka注册中心

---
+++ springboot14-springcloud/eureka-server/src/main/java/com/at/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.at.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 注册中心
+ * 1、配置Eureka信息
+ * 2、@EnableEurekaServer
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
