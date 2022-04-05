7e451cbcc340fe9267f0877d7b10bd2d4a6a2c83
qinmb812 <2449338367@qq.com>
2022-04-05 21:28:59 +0800

    SpringBoot之监管--自定义HealthIndicator

--- springboot16-actuator/src/main/java/com/at/actuator/Springboot16ActuatorApplication.java
+++ springboot16-actuator/src/main/java/com/at/actuator/Springboot16ActuatorApplication.java
@@ -3,6 +3,12 @@ package com.at.actuator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 自定义健康状态指示器
+ * 1、编写一个指示器 实现HealthIndicator接口
+ * 2、指示器的名字 xxxxHealthIndicator
+ * 3、加入容器中
+ */
 @SpringBootApplication
 public class Springboot16ActuatorApplication {
     public static void main(String[] args) {
