b9cdc965c78e73c87edc28a9bfdbf2f686c23468
qinmb812 <2449338367@qq.com>
2022-04-05 18:27:53 +0800

    SpringBoot之热部署--devtools开发热部署

---
+++ springboot15-deploy/src/test/java/com/example/deploy/Springboot15DeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot15DeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
