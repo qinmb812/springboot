bdd3a37753ee1549cf983694d0060969791c3692
qinmb812 <2449338367@qq.com>
2022-04-05 21:04:24 +0800

    SpringBoot之监管--监管端点测试

---
+++ springboot16-actuator/src/main/java/com/at/actuator/Springboot16ActuatorApplication.java
@@ -0,0 +1,11 @@
+package com.at.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot16ActuatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot16ActuatorApplication.class, args);
+    }
+}
---
+++ springboot16-actuator/src/test/java/com/at/actuator/Springboot16ActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.at.actuator;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Springboot16ActuatorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
